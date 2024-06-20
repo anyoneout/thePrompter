4d64c269229798ec75f848c9ea5623888beea064
anyoneout <chrisdafur@gmail.com>
2024-06-20 12:34:05 -0500

    corrected spelling erorr

--- madlibMaker.js
+++ madlibMaker.js
@@ -11,7 +11,7 @@ function madlibMakerRun() {
     adjective = prompt("Write an adjective");
     adverb = prompt("Write an adverb");
     madlibMakerHeader.innerHTML = `${name}'s Madlib`;
-    madlibMakerParagraph.innerHTML = `It was three days ago when I went to the ${noun}. While there I ${verb}ed in the most ${adjective} manner. I think I'll do that again ${adverb}.`;
+    madlibMakerParagraph.innerHTML = `It was three days ago when I went to the ${noun}. While there I ${verb} in the most ${adjective} manner. I think I'll do that again ${adverb}.`;
 }
 
 madlibMakerButton.onclick = madlibMakerRun;
